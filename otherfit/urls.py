53d10ec5727b013f5de100002f378d63935a4bd6
Michel Willians <michel_wrq@hotmail.com>
2019-05-10 11:39:54 -0300

    Ajustar e preparar ambiente Django

--- otherfit/urls.py
+++ otherfit/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from website import views
+from painel.views import perfil
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index)
+    path('', views.index),
+    path('perfil/', perfil)
+
 ]
